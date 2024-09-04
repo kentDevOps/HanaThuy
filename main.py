e2f8c09181e43aa531bf980ded282fdd83d764ca
kentDevOps <data.quaza.3@gmail.com>
2024-09-04 17:43:22 +0700

    Update Code Ver1

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ df = pd.read_excel(file_paths[0])
 result = df.groupby(['Mã sản phẩm', 'Mã NPL'], as_index=False).agg({'Lượng NL, VT thực tế sử dụng để sản xuất một sản phẩm ': 'sum'})
 print(result)
 colNpl = result.iloc[:,0]
+colSoLuong = result.iloc[:,2]
 fileReport = 'vd.xlsx'
-dfReport = pd.read_excel(fileReport,sheet_name='rp')
+dfReport = pd.read_excel(fileReport,sheet_name='vd')
 lastRowRp = len(dfReport) + 1
 print(dfReport)
-df.iloc[20:lastRowRp-2,:] = np.nan
+dfReport.iloc[:,:] = np.nan
+print(dfReport)
 # Ghi vào một trang tính cụ thể 
 with pd.ExcelWriter(r'D:\02_Study\08_Python\16_Hana_Report\HanaThuy\vd.xlsx' , engine='openpyxl', mode='a',if_sheet_exists='overlay') as writer:
-    df.to_excel(writer, sheet_name='vd', index=False, startcol= 0,startrow=23, header=False) 
-    colNpl.to_excel(writer, sheet_name='Sheet1', index=False, startcol= 6, header=False) 
+    dfReport.to_excel(writer, sheet_name='vd', index=False, startcol= 0,startrow=22, header=False) 
+    colNpl.to_excel(writer, sheet_name='vd', index=False, startcol= 1,startrow=22, header=False)  
+    colSoLuong.to_excel(writer, sheet_name='vd', index=False, startcol= 5,startrow=22, header=False)  
